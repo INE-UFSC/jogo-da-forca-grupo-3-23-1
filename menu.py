cb375f5428acfa8ac471689a8482744511bdda4e
Davi Junkes <davimenegazjunkes@hotmail.com>
2023-03-16 22:07:37 -0300

    menu adicionado

--- menu.py
+++ menu.py
@@ -5,12 +5,13 @@ class Menu():
         pass
 
     def iniciar(self):
-        
+        print("Jogo da forca")
+        print("1. Jogar.")
+        print("2. Sair.")
+        option = input("Digite o que voce deseja fazer.")
         while True:
-            print("Jogo da forca")
-            print("1. Jogar.")
-            print("2. Sair.")
-            option = input("Digite o que voce deseja fazer.")
+            
+            #jogo
             if option == "1":
                 palavra = input("Digite a palavra desejada para o jogo.")
                 jogo = Jogo(palavra)
@@ -21,13 +22,31 @@ class Menu():
 
                 if jogo.ganhou:
                     print('Voce ganhou')
-                    
+                    again = input("Deseja jogar novamente? (y/n)")
+                    if again == "y":
+                        option = "3"
+                    else:
+                        break
                 else:
                     print('Voce perdeu')
+                    again = input("Deseja jogar novamente? (y/n)")
+                    if again == "y":
+                        option ="3"
+                    else:
+                        break
 
-
+            #sai do jogo
             elif option == "2":
                 break
+
+            #volta para o menu apos um jogo
+            elif option == "3": 
+                print("Jogo da forca")
+                print("1. Jogar.")
+                print("2. Sair.")
+                option = input("Digite o que voce deseja fazer.")
+
+            #opcao de contra erros
             else:
                 print("1. Jogar.")
                 print("2. Sair.")
